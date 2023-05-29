4fc9958f422505c591b7aaa39cf3006b1e48510c
Andres Paiz <53914150+andreluchi@users.noreply.github.com>
2023-05-28 23:19:09 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "my-app-service-plan"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                      = "my-function-app"
+  location                  = "West US"
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  runtime                   = "dotnet-isolated"
+  os_type                   = "windows" # Or "linux" based on your needs
+  version                   = "~3"
+  dynamic "identity" {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "my-key-vault"
+  location                    = "West US"
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "my-sql-server"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = "West US"
+  version                      = "12.0"
+  administrator_login          = "adminlogin"
+  administrator_login_password = "password"
+}
+
+output "function_app_endpoint" {
+  value = azurerm_function_app.example.default_hostname
+}
+
